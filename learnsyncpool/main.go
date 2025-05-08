887d156aae348ad2f69bc8992bcf1c0b1cb23886
boomxy <2794865850@qq.com>
2025-05-08 19:23:25 +0800

    增加 learnwg/main.go wg.Wait注释， 新增 learnsyncpool 项目

---
+++ learnsyncpool/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+// 1. 创建一个对象池，使用&sync.Pool{}创建一个对象池
+var pool = &sync.Pool{
+	New: func() any {
+		log.Println("create a new object")
+		b := make([]byte, 1024)
+		return &b
+	},
+}
+
+func main() {
+
+	// 一个使用sync.Pool的例子
+	log.Println("main start")
+	fmt.Println("这是一个使用sync.Pool的例子")
+
+	// 2. 获取一个对象，如果对象不存在，会调用New函数创建一个
+	obj := pool.Get().(*[]byte) // 2.1 类型断言.(*[]byte) 为 *[]byte 指针类型
+
+	// 3. 确保使用完对象后，将对象放回对象池
+	defer pool.Put(&obj)
+
+	// 4. 清空对象池
+	*obj = (*obj)[:0]
+
+	// 5. 使用对象
+	*obj = append(*obj, "hello world"...)
+	fmt.Println("对象池内容:", string(*obj))
+
+
+	// obj = append(*obj, "hello world"...) // "hello world"... 是一个切片字面量，表示一个字符串切片
+	// fmt.Println("对象池内容:", string(obj))
+	log.Println("main end")
+
+	os.Exit(0)
+}
--- learnwg/main.go
+++ learnwg/main.go
@@ -16,8 +16,6 @@ func main() {
 	// 2. 计数器加 2， 表示要等待两个 goroutine 完成。
 	wg.Add(2)
 
-	// defer wg.Wait() // 不生效，defer 语句在 wg.Wait() 之前执行，所以不会等待
-
 	// 3. 启动两个 goroutine
 	go func() {
 		defer wg.Done() // 计数器 -1
@@ -29,6 +27,9 @@ func main() {
 	}()
 	// 4. 等待所有 goroutine 完成
 	wg.Wait()
+	// 等待所有goroutine完成，后边没有其他代码的时候可以不用 defer wg.Wait()
+	// 如果我们把Wait()放在defer语句中，那么Wait()会在main()函数返回之前执行。
+	//defer wg.Wait()
 	log.Println("All goroutines finished.")
 	// 5. 退出程序
 	os.Exit(0)
